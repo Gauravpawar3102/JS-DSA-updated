333c4b6d3cc07b9382cf1b4e43a258b13c2be350
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-04-09 10:59:32 +0530

    sum of even numbers

--- Basics-phase-1/loops/ifElse.js
+++ Basics-phase-1/loops/ifElse.js
@@ -1,11 +1,44 @@
-let a, b;
+// let a, b;
 
-function conditional(a, b) {
-  if (a > b) {
-    console.log(a + '  is greater than  ' + b);
-  } else {
-    console.log(b + '  is greater than  ' + a);
+// ?  If else
+
+// function conditional(a, b) {
+//   console.log('enter the value of a and b');
+
+//   if (a > b) {
+//     console.log(a + '  is greater than  ' + b);
+//   } else {
+//     console.log(b + '  is greater than  ' + a);
+//   }
+// }
+
+// conditional(10, 12);
+
+// ? while loop
+
+// let i = 0;
+// let a = 10;
+// function whileloo(a) {
+//   while (i <= a) {
+//     console.log(i);
+//     i+1;
+//   }
+// }
+// whileloo(9);
+
+// ? sum of  N even numbers
+
+let num;
+function sumOfNNum(num) {
+  let sum = 0;
+  let i = 1;
+  for (i; i <= num; i++) {
+    if (i % 2 == 0) {
+      sum = sum + i;
+      console.log(sum);
+    }
   }
+  console.log(sum);
 }
 
-conditional(10, 12);
+sumOfNNum(100);
