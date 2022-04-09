f63451117577932f2781b2f513d3e2ae0f96b7bf
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-04-09 07:06:26 +0530

    if else basic fun

---
+++ Basics-phase-1/loops/ifElse.js
@@ -0,0 +1,11 @@
+let a, b;
+
+function conditional(a, b) {
+  if (a > b) {
+    console.log(a + '  is greater than  ' + b);
+  } else {
+    console.log(b + '  is greater than  ' + a);
+  }
+}
+
+conditional(10, 12);
