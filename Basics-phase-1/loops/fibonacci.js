832f60ddebe879a703cc65fc57e869b9dcf46e59
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-04-12 14:29:47 +0530

    fibonacci

---
+++ Basics-phase-1/loops/fibonacci.js
@@ -0,0 +1,14 @@
+// ? Fibonnaci series
+
+function fibo(n) {
+  let result;
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i <= n; i++) {
+    result = a + b;
+    console.log(result);
+    a = b;
+    b = result;
+  }
+}
+fibo(5);
