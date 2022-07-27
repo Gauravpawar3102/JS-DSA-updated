ab24339ae4a07a32ca2c52f1af15a5df015d50ba
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-07-27 16:04:49 +0530

    2.0

--- Basics-phase-1/leetCode/1.TwoSum.js
+++ Basics-phase-1/leetCode/1.TwoSum.js
@@ -2,10 +2,12 @@ function twoSum(nums, target) {
   const numsMap = {};
   for (let i = 0; i < nums.length; i++) {
     console.log(numsMap);
+    //  21-3=18    = {   numsMap.0 =18     }
     const currentMapVal = numsMap[nums[i]];
     // console.log(currentMapVal);
 
     if (currentMapVal >= 0) {
+      // if numberToFind is positive   18 , 0
       return [currentMapVal, i];
     } else {
       const numberToFind = target - nums[i];
