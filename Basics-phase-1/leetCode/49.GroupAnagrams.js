a9d7b958181e60c950e24a1a5087ee5f86cfaf6a
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-08-02 23:23:57 +0530

    mid brute force

--- Basics-phase-1/leetCode/49.GroupAnagrams.js
+++ Basics-phase-1/leetCode/49.GroupAnagrams.js
@@ -1,2 +1,16 @@
-function groupAnagrams(strs) {}
+function groupAnagrams(strs) {
+  const sortedStrs = [];
+  const FinalArray = [];
+  const sortedArray = sortedStrs.sort();
+
+  for (let i = 0; i < strs.length; i++) {
+    sortedStrs.push(strs[i].split('').sort().join(''));
+
+    // if((strs[i].split('').sort().join('').sort() ===    )
+
+    // console.log(strs[i].split('').sort().join(''));
+  }
+
+  console.log(sortedArray);
+}
 groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']);
