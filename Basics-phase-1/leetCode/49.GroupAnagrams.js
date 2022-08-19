5b92d380ac6a572b1b3fc6e299603c30a1191b76
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-08-19 11:50:26 +0530

    leetcode 49 completed

--- Basics-phase-1/leetCode/49.GroupAnagrams.js
+++ Basics-phase-1/leetCode/49.GroupAnagrams.js
@@ -1,16 +1,24 @@
 function groupAnagrams(strs) {
-  const sortedStrs = [];
-  const FinalArray = [];
-  const sortedArray = sortedStrs.sort();
+  const sortedStrs = strs.map((word) => word.split('').sort().join(''));
 
+  const hash = {};
   for (let i = 0; i < strs.length; i++) {
-    sortedStrs.push(strs[i].split('').sort().join(''));
+    if (!hash[sortedStrs[i]]) {
+      hash[sortedStrs[i]] = [strs[i]];
+    } else {
+      hash[sortedStrs[i]].push(strs[i]);
+    }
 
-    // if((strs[i].split('').sort().join('').sort() ===    )
+    // if (
+    //   strs[i].split('').sort().join('') === strs[i++].split('').sort().join('')
+    // ) {
+    //   FinalArray.push(strs[i]);
+    // }else{
+
+    // }
 
     // console.log(strs[i].split('').sort().join(''));
   }
-
-  console.log(sortedArray);
+  return Object.values(hash);
 }
 groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']);
