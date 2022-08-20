402427b8f5a3c630b69b78ccd563857881c8cae1
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-08-20 23:20:47 +0530

    js

---
+++ javascript-marathon interview/Language-fundamentals.js
@@ -0,0 +1,10 @@
+let x = 1;
+console.log(x);
+console.log(typeof x);
+
+console.log(undefined == null);
+console.log(undefined === null);
+
+let z = undefined;
+console.log(z);
+console.log(typeof z);
