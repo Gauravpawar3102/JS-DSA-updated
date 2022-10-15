cc32f7deb4f308d7549b24f271f2b2cab75d8a19
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-10-15 23:14:47 +0530

    optimal solution lc 121

--- Leetcode-KA/slidingWindow/121-bestTimetoBuyandSellStock.js
+++ Leetcode-KA/slidingWindow/121-bestTimetoBuyandSellStock.js
@@ -1,19 +1,32 @@
+// ?Brute force By Gaurav
+// function maxProfit(prices) {
+//   let object = {};
+//   for (i = 0; i < prices.length; i++) {
+//     let n = i + 1;
+//     if (prices[i] < prices[n]) {
+//       for (j = i + 1; j < prices.length; j++) {
+//         let diff = prices[j] - prices[i];
+//         object[diff] = j;
+//         console.log(diff);
+//         console.log(prices[i]);
+//         console.log(object);
+//       }
+//     }
+//   }
+//   console.log(Math.max(...Object.values(object)));
+//   console.log(Math.max(...Object.keys(object)));
+//   console.log(object);
+// }
+// ?Optimal By Love Babbar
 function maxProfit(prices) {
-  let object = {};
-  for (i = 0; i < prices.length; i++) {
-    let n = i + 1;
-    if (prices[i] < prices[n]) {
-      for (j = i + 1; j < prices.length; j++) {
-        let diff = prices[j] - prices[i];
-        object[diff] = j;
-        console.log(diff);
-        console.log(prices[i]);
-        console.log(object);
-      }
-    }
+  let mini = prices[0];
+  let profit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    let diff = prices[i] - mini;
+    profit = Math.max(diff, profit);
+    mini = Math.min(mini, prices[i]);
   }
-  console.log(Math.max(...Object.values(object)));
-  console.log(Math.max(...Object.keys(object)));
-  console.log(object);
+  return profit;
 }
+
 maxProfit([7, 1, 5, 3, 6, 4]);
