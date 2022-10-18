b00cad5f194d00d346a529d19670dc3b607bbaaf
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-10-18 13:33:37 +0530

    optimal completed

--- Leetcode-KA/Arrays and hashing/1-TwoSum.js
+++ Leetcode-KA/Arrays and hashing/1-TwoSum.js
@@ -1,10 +1,27 @@
+// ?Brute force
+// function twoSum(arr, target) {
+//   for (i = 0; i < arr.length; i++) {
+//     for (j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === target) {
+//         console.log([i, j]);
+//       }
+//     }
+//   }
+// }
+// twoSum([3, 2, 4], 6);
+// ?optimal
 function twoSum(arr, target) {
-  for (i = 0; i < arr.length; i++) {
-    for (j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === target) {
-        console.log([i, j]);
-      }
+  let object = {};
+  for (let i = 0; i < arr.length; i++) {
+    let value = arr[i];
+    let diff = target - value;
+    if (object[diff] === undefined) {
+      object[value] = i;
+    } else {
+      [object[diff], i];
+      console.log([object[diff], i]);
     }
   }
+  console.log(object);
 }
-twoSum([3, 2, 4], 6);
+twoSum([3, 2, 3], 6);
