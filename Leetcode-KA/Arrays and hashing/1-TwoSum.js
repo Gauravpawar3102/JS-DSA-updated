58a2aea14b54a3a15b44f54a8accb06b6227a14b
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-10-18 11:39:51 +0530

    brute force 2sum

--- Leetcode-KA/Arrays and hashing/1-TwoSum.js
+++ Leetcode-KA/Arrays and hashing/1-TwoSum.js
@@ -1,14 +1,10 @@
 function twoSum(arr, target) {
-  const object = {};
-  for (let i = 0; i < arr.length; i++) {
-    const currentMapValue = object[arr[i]];
-    if (currentMapValue >= 0) {
-      return [currentMapValue, i];
-    } else {
-      const numberToFind = target - currentMapValue;
-      object[numberToFind] = i;
+  for (i = 0; i < arr.length; i++) {
+    for (j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        console.log([i, j]);
+      }
     }
   }
-  return null;
 }
-twoSum([2, 7, 11, 15], 9);
+twoSum([3, 2, 4], 6);
